d6e215a438e074cc3fd290dd8bbcd43724c469ac
Shreeyasha Baskota <baskotashreeyasha1@gmail.com>
2020-11-08 08:08:07 +0545

    Rest route explained

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-const http = require('http');
+// const http = require('http');
+// const url= require('url');
 
-let server = http.createServer((request,response)=>{
-    if(request.url === '/'){
-        response.writeHead(200,{'content-type':'text/html'});
-        response.write('<html><body><h1>Hello world</h1><p>This is root page</p></body></html>');
-        response.end();
-    }else if(request.url === '/profile') {
-            response.writeHead(200, {'content-type' : 'text/html'});
-            response.write('<html><body><h1>Hello IIMS</h1><p>This is profile page</p></body></html>');
-            response.end();
-    }else{
-        response.writeHead(404,{'content-type':'text/html'});
-        response.write('<html><body><h1>404 Page not found </h1> </body></html>')
-        response.end();
-    }
-});
+// let server = http.createServer((request,response)=>{
+//     let queryObject=url.parse(request.url,true).query;
+//     if(request.url === '/'){
+//         response.writeHead(200,{'content-type':'text/html'});
+//         response.write('<html><body><h1>Hello ${queryObject.name}</h1><p>This is profile page</p></body></html>');
+//         response.end();
+    // }else if(request.url === '/profile') {
+    //         response.writeHead(200, {'content-type' : 'text/html'});
+    //         response.write('<html><body><h1>Hello IIMS</h1><p>This is profile page</p></body></html>');
+    //         response.end();
+    // }else{
+    //     response.writeHead(404,{'content-type':'text/html'});
+    //     response.write('<html><body><h1>404 Page not found </h1> </body></html>')
+    //     response.end();
+    // }
+// });
 
-server.listen(8000);
-console.log('Server is running on port:',8000);
+// server.listen(8000);
+// console.log('Server is running on port:',8000);
+
+
+// let blog={
+//     title:'',
+//     author:'',
+//     userId:'',
+//     created_at:'',
+//     updated_at:'',
+// }
+
+
+REST API 
+Routes            |   RequestMethod |    Description 
+/blog                    GET             (query get all blogs);
+/blog                    POST            (store a blog);
+/blog/:id                 PUT/PATCH       (update a blog)
+/blog/:id                 DELETE          (delete a blog with id)
+/blog/:id                 GET             (get a blog by id)
