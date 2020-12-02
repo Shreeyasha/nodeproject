da1da1832db4518cce191088751dfbd5e90fe2d6
Shreeyasha Baskota <baskotashreeyasha1@gmail.com>
2020-12-02 10:23:36 +0545

    Simple node server created

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-// const http = require('http');
-// const url= require('url');
+const http = require('http');
+const url= require('url');
+let server = http.createServer((request,response)=>{
+  let queryObject = url.parse(request.url,true).query;
+  response.writeHead(200,{'content-type':'text/html'})
+    response.write(`<html><body><h1>Welcome to my profile ${queryObject.name}. Your age is ${queryObject.age}</h1></body></html>`)
+    response.end();
 
-// let server = http.createServer((request,response)=>{
-//     let queryObject=url.parse(request.url,true).query;
-//     if(request.url === '/'){
-//         response.writeHead(200,{'content-type':'text/html'});
-//         response.write('<html><body><h1>Hello ${queryObject.name}</h1><p>This is profile page</p></body></html>');
-//         response.end();
-    // }else if(request.url === '/profile') {
-    //         response.writeHead(200, {'content-type' : 'text/html'});
-    //         response.write('<html><body><h1>Hello IIMS</h1><p>This is profile page</p></body></html>');
-    //         response.end();
-    // }else{
-    //     response.writeHead(404,{'content-type':'text/html'});
-    //     response.write('<html><body><h1>404 Page not found </h1> </body></html>')
-    //     response.end();
-    // }
-// });
+  // if (request.url==='/'){
+  //   response.writeHead(200,{'content-type':'text/html'})
+  //   response.write('<html><body><h1>This is route page </h1></body></html>')
+  //   response.end();
 
-// server.listen(8000);
-// console.log('Server is running on port:',8000);
+  // }
+  // else if(request.url==='/profile'){
+  //   response.writeHead(200,{'content-type':'text/html'})
+  //   response.write('<html><body><h1>Welcome to my profile </h1></body></html>')
+  //   response.end();
+
+  // }
+  // else{
+  //   response.writeHead(404,{'content-type':'text/html'})
+  //   response.write('<html><body><h1>Page not found  </h1></body></html>')
+  //   response.end();
+
+  // }
+
+});
+server.listen(8000);
+console.log("Server is running on port",8000)
+
+
+
+
+
+// -----
 
 
 // let blog={
@@ -31,10 +45,63 @@
 // }
 
 
-REST API 
-Routes            |   RequestMethod |    Description 
-/blog                    GET             (query get all blogs);
-/blog                    POST            (store a blog);
-/blog/:id                 PUT/PATCH       (update a blog)
-/blog/:id                 DELETE          (delete a blog with id)
-/blog/:id                 GET             (get a blog by id)
+// REST API 
+// Routes            |   RequestMethod |    Description 
+// /blog                    GET             (query get all blogs);
+// /blog                    POST            (store a blog);
+// /blog/:id                 PUT/PATCH       (update a blog)
+// /blog/:id                 DELETE          (delete a blog with id)
+// /blog/:id                 GET             (get a blog by id)
+
+// const express = require('express');
+
+// const app = express();
+// const port = 8000;
+
+// let blogs = [{
+// 	id: 1,
+// 	title: 'hello'
+// },
+// {
+// 	id: 2,
+// 	title: 'world'
+// }];
+
+
+// app.get('/blog',(req, resp) => {
+// 	resp.json(blogs);
+// });
+
+// app.post('/blog',(req, res) => {
+// 	res.json({method: req.method});
+// });
+
+// app.put('/blog/:id',(req, res) => {
+// 	res.json({method: req.method});
+// });
+
+// app.get('/blog/:id', (req, res) => {
+// 	let data = blogs.filter((value) => value.id === parseInt(req.params.id));
+// 	if(!data[0]){
+// 		return res.status(404).json({error: 'Data not found'});
+// 	}
+// 	res.json(data[0]);
+// })
+
+// app.delete('/blog/:id', (req, res) => {
+// 	res.json({method: req.method});
+// })
+
+// app.listen(port, () => {
+// 	console.log(`Application is running on port: ${port}`);
+// })
+
+// const mongoose = require('mongoose');
+
+
+// mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('Mongodb connected');
+// });
