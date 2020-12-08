3662870244241ca602bc751e049d4f51bf1c89f2
Shreeyasha Baskota <baskotashreeyasha1@gmail.com>
2020-12-08 19:20:26 +0545

    REST API with CRUD operation

--- index.js
+++ index.js
@@ -81,18 +81,32 @@ app.get('/blog',(req, resp) => {
 	resp.json(blogs);
 });
 
+	
+
 // store a blog
-app.post('/blog',(req, res) => {
-	res.json({method: req.method});
-	// let {title} = req.body;
-	let title = req.body.title;
-	let data = {
-		id: blogs.length + 1,
-		title: title
-	}
-	blogs.push(data)
-	res.json(data);
-});
+// app.post('/blog',(req, res) => {
+// 	res.json({method: req.method});
+// 	// let {title} = req.body;
+// 	let title = req.body.title;
+// 	let data = {
+// 		id: blogs.length + 1,
+// 		title: title
+// 	}
+// 	blogs.push(data)
+// 	res.json(data);
+// });
+
+	app.post('/blog',(req,res)=>{
+		let {title}= req.body
+		let data ={
+			id: blogs.length+ 1,
+			title
+		}
+		blogs.push(data);
+		res.json(data)
+	})
+	
+//  update a blog by id
 
 
 app.put('/blog/:id',(req, res) => {
@@ -113,30 +127,87 @@ app.put('/blog/:id',(req, res) => {
 	res.json({id: req.params.id, title:title});
 });
 
+// 	app.put('/blog/:id',(req,res)=>{
 
+// 		let blog = blogs.filter((value,index)=>{
+// 			return value.id=== parseInt(req.params.id)
+// 		});
+// 		if(!blog[0]){
+// 			res.status(404).json({error:'Not found'})
+// 		}
+// 		let {title}= req.body
+// 		blogs.forEach((value,index)=>{
+// 			if(value.id=== parseInt(req.params.id)){
+// 				blogs[index]= title
+// 			}
+// 		})
+// 			res.json({id: req.params.id, title})
+
+// 	})
+
+// get a blog by id 
 
 app.get('/blog/:id', (req, res) => {
-	let data = blogs.filter((value) => value.id === parseInt(req.params.id));
-	if(!data[0]){return res.status(404).json({error: 'Data not found'});
+	let data = blogs.filter((value) => 
+	value.id === parseInt(req.params.id)
+	);
+	if(!data[0]){
+		return res.status(404).json({error: 'Data not found'});
 }
 res.json(data[0]);
 });
-app.delete('/blog/:id', (req, res) => {
-	res.json({method: req.method});
-	let blog = blogs.filter((value, index) => {
-		return value.id === parseInt(req.params.id)
-	});
-	if(!blog[0]){
-		return res.status(404).json({error: 'Data not found'});
+
+	// app.get('/blog/:id',(req,res)=>{
+	// 	let blog = blogs.filter((value)=>{
+	// 		value.id=== parseInt(req.params.id)
+	// 	})
+	// 	if(!data[0]){
+	// 		return res.status(404).json({error:'data not found'})
+	// 	}
+	// 	res.json(data[0]);
+	// })
+
+// delete a blog 
+// app.delete('/blog/:id', (req, res) => {
+// 	res.json({method: req.method});
+// 	let blog = blogs.filter((value, index) => {
+// 		return value.id === parseInt(req.params.id)
+// 	});
+// 	if(!blog[0]){
+// 		return res.status(404).json({
+// 			error: 'Data not found'
+// 		});
+// 	}
+// 	let deleteIndex;
+// 	blogs.forEach((value, index) => {
+// 		if(value.id === parseInt(req.params.id)){
+// 			deleteIndex = index;
+// 		}
+// 	})
+// 	blogs.splice(deleteIndex, 1);
+// 	res.status(204).json({});
+// })
+
+app.delete('/blog/:id',(req,res)=>{
+
+	let blog= blogs.filter((value,index)=>{
+		return value.id===parseInt(req.params.id);
+	})
+	if (!blogs[0]){   
+		// this also refers to (!blogs.length===0)
+		return res.status(404).json({error:'Data not found'})
 	}
 	let deleteIndex;
-	blogs.forEach((value, index) => {
-		if(value.id === parseInt(req.params.id)){
-			deleteIndex = index;
+	blogs.filter((index,value)=>{
+		if(value.id=== parseInt(req.params.id)){
+			deleteIndex=index;
 		}
 	})
-	blogs.splice(deleteIndex, 1);
-	res.status(204).json({});
+	blogs.splice(deleteIndex,1);
+	res.status(204).json({})
 })
-app.listen(port, () => {console.log(`Application is running on port: ${port}`);
+
+
+app.listen(port, () => {
+	console.log(`Application is running on port: ${port}`);
 })
